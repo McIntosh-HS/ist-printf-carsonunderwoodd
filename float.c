3c06d4614d7c22324ded1571aa893037109b9557
carson underwood <2027underwood.carson@fcboe.org>
2024-01-19 14:30:51 -0500

    yay

---
+++ float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  float subtotal = 23.84;
+  float tax = .09;
+  
+  printf("How much are your groceries? ");
+  scanf("%f", &subtotal);
+  printf("How much are yorr groceries"); // ask the user for the tax rate
+  scanf("%f", &tax); // put user input in tax variable
+  printf("The final total is: %.2f\n", subtotal + subtotal*tax);
+  return 0;
+}
